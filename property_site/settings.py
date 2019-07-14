04f6d00c4cb11ab20448ca6afbea32005bd32d85
Steve McLoughlin <steve.mcloughlin@hotmail.co.uk>
2019-07-14 11:43:38 +0100

    Database created and connection setup completed with migrations

--- property_site/settings.py
+++ property_site/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'property_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'Letmein2393!',
+        'HOST': 'localhost',
+        'PORT': '5433'
     }
 }
 
